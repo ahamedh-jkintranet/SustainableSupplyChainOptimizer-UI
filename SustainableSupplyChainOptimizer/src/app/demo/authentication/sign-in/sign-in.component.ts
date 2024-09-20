4d9f71765ed20a8645b3b652481ce1d49104fa6b
Samila Chanuka <samilac@jkintranet.com>
2024-09-20 13:03:57 +0530

    Added the input feild validation for the Sign-In page.

--- SustainableSupplyChainOptimizer/src/app/demo/authentication/sign-in/sign-in.component.ts
+++ SustainableSupplyChainOptimizer/src/app/demo/authentication/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 // angular import
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 // project import
 import { SharedModule } from 'src/app/theme/shared/shared.module';
@@ -12,4 +12,17 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export default class SignInComponent {}
+export default class SignInComponent {
+  constructor(private router : Router) {}
+
+  password: string = '';
+  username: string = '';
+
+  signIn() : void {
+    if (this.username && this.password)
+    {
+      this.router.navigate(['/home']);
+    }
+  }
+  
+}
