4cf59db4f76121dd6ad63ff5c7cffa9bde55ceb8
Dushyanth Kandiah <dushyanthk@jkintranet.com>
2024-09-20 11:27:43 +0530

    SL map

--- SustainableSupplyChainOptimizer/src/app/cf-analysis/sl-map/sl-map.component.ts
+++ SustainableSupplyChainOptimizer/src/app/cf-analysis/sl-map/sl-map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import * as  $ from 'jquery'
 @Component({
   selector: 'app-sl-map',
@@ -8,23 +8,73 @@ import * as  $ from 'jquery'
 })
 export class SlMapComponent {
 
+  @Input() localData;
+  ungrouped = []
+
+  emissionsByDistrict = []
+
 
   constructor() {
 
   }
 
   ngOnInit() {
+    let jsonArr = JSON.parse(this.localData) as any[]
 
-    function showTooltip(evt, text) {
-      debugger
+    jsonArr.forEach(x => {
+      let combined = [...this.ungrouped, ...x.companyDataList];
+      this.ungrouped = combined;
+    })
 
-    }
+    let res = this.ungrouped.reduce((acc, { district, emissions }) => {
+      if (!acc[district]) {
+        acc[district] = 0;
+      }
+      acc[district] += emissions;
+      return acc;
+    }, {});
+
+    let rawData: any = Object.entries(res).map(([key, value]) => {
+      if (typeof value === 'number') {
+        return { key, value: parseFloat(value.toFixed(2)) };
+      }
+      return { key, value };
+    });
+
+    // Sort the data in descending order based on value
+    const sortedData = [...rawData].sort((a, b) => b.value - a.value);
+
+    // Determine the length of each segment
+    const segmentLength = Math.ceil(sortedData.length / 3);
+
+    // Create the color mapping
+    this.emissionsByDistrict = sortedData.map((item, index) => {
+      let color;
+      if (index < segmentLength) {
+        color = 'red'; // highest values
+      } else if (index < segmentLength * 2) {
+        color = 'yellow'; // medium values
+      } else {
+        color = 'green'; // lowest values
+      }
+
+      return { ...item, color };
+    });
+
+    console.log(this.emissionsByDistrict);
+
   }
 
   showTooltip(evt, text) {
-
+    let emission = this.emissionsByDistrict.find(x => x.key == text)
     let tooltip = document.getElementById("tooltip");
-    tooltip.innerHTML = text;
+    tooltip.innerHTML = ` 
+      <div class="p-4 text-start">
+       <h6>District : <b class='font-weight-bold'>${text}</b></h6>
+
+       <h6>Total Emission : <b class='font-weight-bold'>${emission.value}</b></h6>
+    </div>
+    `;
     tooltip.style.display = "block";
     tooltip.style.left = evt.layerX + 10 + 'px';
     tooltip.style.top = evt.layerY + 10 + 'px';
