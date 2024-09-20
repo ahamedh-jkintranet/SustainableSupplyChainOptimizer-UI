458aa25b432d8c7de90688d2fb468c05cd056b6a
Samila Chanuka <samilac@jkintranet.com>
2024-09-20 11:38:53 +0530

    Added the UI screens for Supplier Evaluation & Route Optimization screens.

--- SustainableSupplyChainOptimizer/src/app/supplier-evaluation/supplier-evaluation.component.ts
+++ SustainableSupplyChainOptimizer/src/app/supplier-evaluation/supplier-evaluation.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupplierService } from '../services/suppiler.service';
+import { SharedModule } from '../theme/shared/shared.module';
 
 @Component({
   selector: 'app-supplier-evaluation',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './supplier-evaluation.component.html',
   styleUrl: './supplier-evaluation.component.scss'
 })
-export class SupplierEvaluationComponent {
+export class SupplierEvaluationComponent implements OnInit{
+  supplierData: any[] = [];
 
+ constructor(private _supplierService: SupplierService) {}
+
+  ngOnInit(): void {
+    this.loadSupplierTableData();
+  }
+
+  loadSupplierTableData() : any {
+    this._supplierService.getSupplierEvaluationProfile().subscribe(
+      (response: any[]) => {
+          this.supplierData = response;
+          console.log('this.supplierData:', this.supplierData);
+      },
+      (error) => console.error('Error fetching dashboard profiles:', error),
+    );
+  }
 }
